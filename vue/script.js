7816255269a3f309b650b3c395ee4afa166c087a
Matteo <matteomigliori280489@gmail.com>
2021-04-13 08:43:09 +0200

    header + 1 section

---
+++ vue/script.js
@@ -0,0 +1,15 @@
+let app = new Vue ({
+    el: '#app',
+    data: {
+      /* header e footer*/
+      navLinks: [
+        'home',
+        'about',
+        'project',
+        'services',
+        'blog',
+        'contact'
+      ],
+      /* / header e footer*/
+    }
+})
